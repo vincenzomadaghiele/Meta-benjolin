3d02aa0fd3fc6cc5d44f01f6bbdcba1fbcac2f3a
Vincenzo Madaghiele <vincenzo.madaghiele@gmail.com>
2024-10-14 14:49:51 +0200

    audio recorder

--- webpd-benjolin-2.0/app/interaction.js
+++ webpd-benjolin-2.0/app/interaction.js
@@ -1,3 +1,5 @@
+//import Recorder from 'recorder-js';
+
 // SUMMARY
 // 1. WEB PAGE INITIALIZATION
 // 2. SENDING MESSAGES FROM JAVASCRIPT TO THE PATCH
@@ -281,3 +283,36 @@ const sendMsgToWebPd = (nodeId, portletId, message) => {
 // ------------- 3. SENDING MESSAGES FROM THE PATCH TO JAVASCRIPT
 // Coming soon ... 
 
+
+
+const recorder = new Recorder(audioContext, {
+  // An array of 255 Numbers
+  // You can use this to visualize the audio stream
+  // If you use react, check out react-wave-stream
+  onAnalysed: data => console.log(data),
+});
+ 
+let isRecording = false;
+let blob = null;
+ 
+navigator.mediaDevices.getUserMedia({audio: true})
+  .then(stream => recorder.init(stream))
+  .catch(err => console.log('Uh oh... unable to get stream...', err));
+ 
+function startRecording() {
+  recorder.start()
+    .then(() => isRecording = true);
+}
+
+function stopRecording() {
+  recorder.stop()
+    .then(({blob, buffer}) => {
+      blob = blob;
+ 
+      // buffer is an AudioBuffer
+    });
+}
+ 
+function download() {
+  Recorder.download(blob, 'my-audio-file'); // downloads a .wav file
+}
